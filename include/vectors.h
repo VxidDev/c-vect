288dc3b0a093e38f73f08354694298fae26af3fa
Void <stas050595@gmail.com>
2026-01-03 17:04:02 +0100

    Updated GetCapacityVec() and added test for it.

--- include/vectors.h
+++ include/vectors.h
@@ -21,6 +21,7 @@ void* GetLastVec(Vector* vec);
 void* PopVec(Vector* vec);
 
 ptrdiff_t GetSizeVec(Vector* vec);
+ptrdiff_t GetCapacityVec(Vector* vec);
 
 void FreeVec(Vector* vec);
 
@@ -30,7 +31,6 @@ bool ResetVec(void* vec , enum VecType type);
 bool ShrinkToFitVec(void* vec , enum VecType type);
 bool ShrinkVec(void* vec , size_t size , enum VecType type);
 bool ExtendVec(void* vec , size_t size , enum VecType type);
-ptrdiff_t GetCapacityVec(void* vec , enum VecType type);
 bool InsertVec(void* vec, void* item, size_t index , enum VecType type);
 bool SetItemVec(void* vec, void* item, size_t index, enum VecType type);
 bool ContainsVec(void* vec, void* item, enum VecType type);
